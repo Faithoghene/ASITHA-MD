7af6d43ef0a39b8400b5b9316ce55f53ed739fe6
Faithoghene <faithoghene17@gmail.com>
2024-10-15 00:33:02 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=ymYwVTTD#RozKd2OxLiniMNi5ypPwRXPr4Ey13vVt6I3k1k83d6o",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
 };
